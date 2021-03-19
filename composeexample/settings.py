376d324c9084c5fca9aaf398ffaf1a8f6c54e7a3
Scott Hurst <scohurst@amazon.com>
2021-03-19 15:12:04 -0700

    Removed docker-compose.yml, switched to SQLite3 to avoid a multicontainer app.

--- composeexample/settings.py
+++ composeexample/settings.py
@@ -75,12 +75,8 @@ WSGI_APPLICATION = 'composeexample.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
